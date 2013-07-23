82e470695c1a66d9250711e50a1de5e94eab655f
Mike Pretzlaw <pretzlaw@gmail.com>
2013-07-23 11:13:47 +0200

    Rewrite Sales_Qoute_Address

---
+++ app/code/community/Webguys/Telefonkeinpflicht/Model/Sales/Quote/Address.php
@@ -0,0 +1,26 @@
+<?php
+
+class Webguys_Telefonkeinpflicht_Model_Sales_Quote_Address extends Mage_Sales_Model_Quote_Address {
+    /**
+     * Validate address attribute values
+     *
+     * @return bool
+     */
+    public function validate()
+    {
+        $errors = parent::validate();
+
+        $messageToFilter = Mage::helper('customer')->__('Please enter the telephone number.');
+        if (is_array($errors) && false !== ($x = array_search($messageToFilter, $errors)))
+        { // found telephone error: get rid of it!
+            unset($errors[$x]);
+        }
+
+        if ($errors === true || empty($errors))
+        { // no errors: be true
+            return true;
+        }
+
+        return $errors;
+    }
+}
