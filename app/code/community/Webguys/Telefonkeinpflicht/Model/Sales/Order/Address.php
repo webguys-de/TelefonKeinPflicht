e4abc832cb9ad527b46acbdfae11b66300b89c45
Tobias Vogt <tobias@code-x.de>
2015-09-21 14:47:14 +0200

    Event nachtraeglich werfen

---
+++ app/code/community/Webguys/Telefonkeinpflicht/Model/Sales/Order/Address.php
@@ -0,0 +1,30 @@
+<?php
+
+class Webguys_Telefonkeinpflicht_Model_Sales_Order_Address extends Mage_Sales_Model_Order_Address
+{
+
+    /**
+     * Validate address attribute values
+     *
+     * @return bool
+     */
+    public function validate()
+    {
+        $errors = parent::validate();
+
+        $messageToFilter = Mage::helper('customer')->__('Please enter the telephone number.');
+        if (is_array($errors) && false !== ($x = array_search($messageToFilter, $errors)))
+        { // found telephone error: get rid of it!
+            unset($errors[$x]);
+            Mage::dispatchEvent('customer_address_validation_after', array('address' => $this));
+        }
+
+        if ($errors === true || empty($errors))
+        { // no errors: be true
+            return true;
+        }
+
+        return $errors;
+    }
+
+}
